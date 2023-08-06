3f1eea59003cdc394ffe13378a38d011663d3f4a
Chris <chris.gironi@me.com>
2023-08-06 17:23:15 +0100

    Increased waitress threads (max connections)

--- app.py
+++ app.py
@@ -1180,5 +1180,5 @@ def lineup():
 
 if __name__ == "__main__":
     config = loadConfig()
-    waitress.serve(app, port=8001, _quiet=True)
+    waitress.serve(app, port=8001, _quiet=True, threads=24)
     # app.run(host="0.0.0.0", port=8001, debug=debug)
